aad33939041298759eb99446f9f27c4c04290e57
Vadim_Vinogradov <voldo90@inbox.ru>
2023-04-15 01:56:49 +0300

    Решение задачи 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl(new UserDaoJDBCImpl());
+
+        userService.createUsersTable();
+
+        userService.saveUser("Vadim", "Vinogradov", (byte) 32);
+        userService.saveUser("Polina", "Tikhova", (byte) 23);
+        userService.saveUser("Vladimir", "Semennikov", (byte) 35);
+        userService.saveUser("Kristina", "Semennikova", (byte) 32);
+
+        List<User> list = userService.getAllUsers();
+        for (User i: list) {
+            System.out.println(i);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,110 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
-
+    }
+    public void createUsersTable() throws SQLException {
+        Connection connection = Util.getConnection();
+        try (Statement statement = connection.createStatement()) {
+            String mysql = "CREATE table if not exists Users (id bigint primary key auto_increment, " +
+                    "name varchar(255), " +
+                    "lastName varchar(255), " +
+                    "age tinyint)";
+            statement.execute(mysql);
+        } catch (SQLException e) {
+            System.out.println("Такая таблица уже была создана");
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+    public void dropUsersTable() throws SQLException {
+        Connection connection = Util.getConnection();
+        String mysql = "DROP TABLE USERS";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(mysql);
+        } catch (SQLException e) {
+            System.out.println("Такой таблицы не существует");
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        Connection connection = Util.getConnection();
+        String mysql = "INSERT INTO USERS (NAME, LASTNAME, AGE) VALUES (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(mysql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
-    public void createUsersTable() {
-
+    public void removeUserById(long id) throws SQLException {
+        Connection connection = Util.getConnection();
+        String mysql = "DELETE FROM USERS WHERE ID = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(mysql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
-
-    public void dropUsersTable() {
-
+    public List<User> getAllUsers() throws SQLException {
+        Connection connection = Util.getConnection();
+        List<User> userList = new ArrayList<>();
+        String mysql = "SELECT * FROM USERS";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(mysql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setLastName(resultSet.getString("LASTNAME"));
+                user.setAge(resultSet.getByte("AGE"));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return userList;
     }
-
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
-
-    public void removeUserById(long id) {
-
-    }
-
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    public void cleanUsersTable() {
-
+    public void cleanUsersTable() throws SQLException {
+        Connection connection = Util.getConnection();
+        String mysql = "TRUNCATE TABLE USERS";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(mysql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 }
